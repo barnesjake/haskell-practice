0c9a5d9b2b0c1e4b400cdebf6150e4521e17c6be
jake.barnes <jake.barnes@digital.hmrc.gov.uk>
2018-06-20 09:31:17 +0100

    lists

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ Repo containing exercises from http://learnyouahaskell.com
 ### Haskell compiler
 Download a Haskell compiler - https://www.haskell.org/platform/
 
-### Running scripts
+### Compiling scripts
 In the terminal/console navigate to the directory in which the script is saved, then run "ghci"
-Once in ghc's interactive mode, type the name of the scipt (without the extension)
+Once in ghc's interactive mode, type the name of the scipt (without the extension):
 '''
 :l scriptName
 '''
+Once the compilation is done, you can use the functions within the ghci.
--- scripts/baby.hs
+++ scripts/baby.hs
@@ -1,1 +1,34 @@
 doubleMe x = x + x
+
+doubleUs x y = x*2 + y*2
+
+doubleUsAlt x y = doubleMe x + doubleMe y
+
+-- |In Haskell, the else statement is mandatory
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x*2
+
+-- | Apostraphe (') is a valid character in Haskell,
+-- | used to either denote a strict version of a function (one that isn't lazy)
+-- | or a slightly modified version of a function or a variable
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+
+-- | let a = 1 inside GHCI is the equivalent of writing
+-- | a = 1 in a script and then loading it.
+
+-- | ++ operator appends things together
+-- | [1,2,3,4] ++ [5,6,7]
+-- | [1,2,3,4,5,6,7]
+list1 = [1,2,3]
+list2 = [4,5,6]
+list3 = list1 ++ list2
+-- | : operator puts something at the beginning of a list (cons operator)
+-- | 'A':" small cat"
+-- | "A small cat"
+list4 = 7 : list1
+-- | !! operator extracts an element out of a list
+-- | "Jake Barnes" !! 5
+-- | [1,2,3,4,5,6,7,8,9] !! 3
+val1 = list3 !! 3
