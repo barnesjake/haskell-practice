73432273bd076f64c0b25a3ea0aec5fd62b3ead4
jake.barnes <jake.barnes@digital.hmrc.gov.uk>
2018-06-20 12:25:52 +0100

    More lists

--- scripts/baby.hs
+++ scripts/baby.hs
@@ -32,3 +32,23 @@ list4 = 7 : list1
 -- | "Jake Barnes" !! 5
 -- | [1,2,3,4,5,6,7,8,9] !! 3
 val1 = list3 !! 3
+
+-- | head takes a list and returns its head
+val2 = head [5,4,3,2,1]
+-- | tail takes the list and returns everyhting except the first element
+val3 = tail[1,2,3,4,5]
+-- | last returns the last element in a list
+val4 = last[1,2,3,4,5]
+-- | init takes a list and returns everything except its last element
+val5 = [5,4,3,2,1]
+
+val6 = length[1,2,3,4]
+-- | null returns a boolean whether a list is empty
+val7 = null[1,2,3] -- | False
+val8 = null[] -- | True
+
+val9 = reverse[5,4,3,2,1]
+
+-- | take x, takes the first x elements in the list
+val10 = take 3 [5,4,3,2,1] -- | [5,4,3]
+-- | drop x, drops the first x elements in the list 
